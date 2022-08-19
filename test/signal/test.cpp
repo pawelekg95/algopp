140a70a148d23415d469a06139d1a76724026858
gmurczykpa <pawel.gmurczyk@trumpf.com>
2022-08-19 22:04:21 +0200

    Initial commit

--- test/signal/test.cpp
+++ test/signal/test.cpp
@@ -1,6 +1,6 @@
 #define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this
 // in one cpp file
-#include "catch2/catch.hpp"
+#include <catch2/catch_all.hpp>
 
 #include "algopp/signal/Signal.h"
 #include "algopp/types/Peak.h"
