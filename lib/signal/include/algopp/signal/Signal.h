3a3e7624191b0c3b0f1998d9e6e3844f92264344
gmurczykpa <pawel.gmurczyk@trumpf.com>
2022-08-19 22:18:42 +0200

    Initial commit

--- lib/signal/include/algopp/signal/Signal.h
+++ lib/signal/include/algopp/signal/Signal.h
@@ -7,6 +7,8 @@
 #include <vector>
 #include <list>
 #include <array>
+#include <cstdint>
+#include <limits>
 
 namespace algopp::signal {
 
@@ -14,6 +16,10 @@ class Signal
 {
 public:
     Signal();
+
+    types::Peak peaks(types::PeakType type = types::PeakType::eHigh, long double distance = 0, long double height = std::numeric_limits<long double>::min());
+
+    std::list<types::Point> points() { return m_points; }
 
 private:
     std::list<types::Point> m_points;
