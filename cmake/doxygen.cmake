1580ea4ab49bd0e8cfecc052fd5a3446aee81db8
gmurczykpa <pawel.gmurczyk@trumpf.com>
2022-11-16 23:30:31 +0100

    Doxygen documentation job

---
+++ cmake/doxygen.cmake
@@ -0,0 +1,17 @@
+function(doxygen_add_target)
+    find_package(Doxygen)
+    if (DOXYGEN_FOUND)
+        set(DOXYGEN_OUTPUT_DIRECTORY            ${CMAKE_BINARY_DIR}/docs)
+        set(DOXYGEN_EXTRACT_PRIVATE             YES)
+        set(DOXYGEN_EXTRACT_PACKAGE             YES)
+        set(DOXYGEN_EXTRACT_STATIC              YES)
+        set(DOXYGEN_WARN_NO_PARAMDOC            YES)
+        set(DOXYGEN_HTML_OUTPUT                 .)
+        set(DOXYGEN_USE_MDFILE_AS_MAINPAGE      README.md)
+        set(DOXYGEN_FILE_PATTERNS               *.c *.cc *.cxx *.cpp *.c++ *.ii *.ixx *.ipp *.i++ *.inl *.h *.hh *.hxx *.hpp *.h++ *.inc README.md)
+        set(DOXYGEN_WARN_LOGFILE                ${DOXYGEN_OUTPUT_DIRECTORY}/doxygen.warn)
+
+        # Add target to generate doxygen docs in the build directory.
+        doxygen_add_docs(doxygen ${ARGN} WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
+    endif ()
+endfunction()
