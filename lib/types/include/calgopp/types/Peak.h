a4fdf04a8a3cb3843aa414a79f65f4a67511c561
gmurczykpa <pawel.gmurczyk@trumpf.com>
2022-11-30 16:53:35 +0100

    Filled doc comments

--- lib/types/include/calgopp/types/Peak.h
+++ lib/types/include/calgopp/types/Peak.h
@@ -4,32 +4,69 @@
 
 namespace calgopp::types {
 
+/**
+ * Peak type.
+ */
 enum class PeakType
 {
     eLow,
     eHigh
 };
 
+/**
+ * Peak representation
+ */
 struct Peak : public Point
 {
+    /**
+     * Default constructor.
+     */
     Peak() = default;
 
+    /**
+     * Constructs peak from same numeric type of arguents
+     * @tparam T                    Type of input arguments.
+     * @param x                     X axis value.
+     * @param y                     Y axis value.
+     */
     template <typename T>
     Peak(T x, T y)
         : Point(x, y)
     {}
 
+    /**
+     * Constructs peak from same numeric type of arguments and specified peak type.
+     * @tparam T                    Type of input arguments.
+     * @param x                     X axis value.
+     * @param y                     Y axis value.
+     * @param type                  Peak type.
+     */
     template <typename T>
     Peak(T x, T y, PeakType type)
         : Point(x, y)
         , type(type)
     {}
 
+    /**
+     * Constructs peak from different numeric types of arguments.
+     * @tparam T                    First numeric type
+     * @tparam Y                    Second numeric type
+     * @param x                     X axis value.
+     * @param y                     Y axis value.
+     */
     template <typename T, typename Y>
     Peak(T x, Y y)
         : Point(x, y)
     {}
 
+    /**
+     * Constructs peak from different numeric types of arguments and specified peak type.
+     * @tparam T                    First numeric type
+     * @tparam Y                    Second numeric type
+     * @param x                     X axis value.
+     * @param y                     Y axis value.
+     * @param type                  Peak type.
+     */
     template <typename T, typename Y>
     Peak(T x, Y y, PeakType type)
         : Point(x, y)
@@ -39,12 +76,36 @@ struct Peak : public Point
     PeakType type{PeakType::eHigh};
 };
 
+/**
+ * Equality operator for lvalue reference.
+ * @param lhs                       Left hand object.
+ * @param rhs                       Right hand object.
+ * @return True if peaks equal, false otherwise.
+ */
 bool operator==(const Peak& lhs, const Peak& rhs);
 
+/**
+ * Equality operator for rvalue reference.
+ * @param lhs                       Left hand object.
+ * @param rhs                       Right hand object.
+ * @return True if peaks equal, false otherwise.
+ */
 bool operator==(const Peak&& lhs, const Peak&& rhs);
 
+/**
+ * Inequality operator for lvalue reference.
+ * @param lhs                       Left hand object.
+ * @param rhs                       Right hand object.
+ * @return True if peaks are not equal, false otherwise.
+ */
 bool operator!=(const Peak& lhs, const Peak& rhs);
 
+/**
+ * Inequality operator for rvalue reference.
+ * @param lhs                       Left hand object.
+ * @param rhs                       Right hand object.
+ * @return True if peaks are not equal, false otherwise.
+ */
 bool operator!=(const Peak&& lhs, const Peak&& rhs);
 
 } // namespace calgopp::types
--- lib/types/include/calgopp/types/Point.h
+++ lib/types/include/calgopp/types/Point.h
@@ -2,16 +2,35 @@
 
 namespace calgopp::types {
 
+/**
+ * Point representation
+ */
 struct Point
 {
+    /**
+     * Default constructor
+     */
     Point() = default;
 
+    /**
+     * Constructs point from same numeric type of arguments
+     * @tparam T                    Type of input arguments.
+     * @param x                     X axis value.
+     * @param y                     Y axis value.
+     */
     template <typename T>
     Point(T x, T y)
         : x(double(x))
         , y(double(y))
     {}
 
+    /**
+     * Constructs point from different numeric types of arguments.
+     * @tparam T                    First numeric type
+     * @tparam Y                    Second numeric type
+     * @param x                     X axis value.
+     * @param y                     Y axis value.
+     */
     template <typename T, typename Y>
     Point(T x, Y y)
         : x(double(x))
@@ -21,21 +40,67 @@ struct Point
     long double x{};
     long double y{};
 
+    /**
+     * Appending and assignment operator.
+     * @param other                 Point to add.
+     * @return This with added other point.
+     */
     Point& operator+=(const Point& other);
 
+    /**
+     * Subtraction and assignment operator.
+     * @param other                 Point to subtract.
+     * @return This with subtracted other point.
+     */
     Point& operator-=(const Point& other);
 };
 
+/**
+ * Equality operator for lvalue reference.
+ * @param lhs                       Left hand object.
+ * @param rhs                       Right hand object.
+ * @return True if points equal, false otherwise.
+ */
 bool operator==(const Point& lhs, const Point& rhs);
 
+/**
+ * Equality operator for rvalue reference.
+ * @param lhs                       Left hand object.
+ * @param rhs                       Right hand object.
+ * @return True if points equal, false otherwise.
+ */
 bool operator==(const Point&& lhs, const Point&& rhs);
 
+/**
+ * Inequality operator for lvalue reference.
+ * @param lhs                       Left hand object.
+ * @param rhs                       Right hand object.
+ * @return True if points are not equal, false otherwise.
+ */
 bool operator!=(const Point& lhs, const Point& rhs);
 
+/**
+ * Inequality operator for rvalue reference.
+ * @param lhs                       Left hand object.
+ * @param rhs                       Right hand object.
+ * @return True if points are not equal, false otherwise.
+ */
 bool operator!=(const Point&& lhs, const Point&& rhs);
 
+/**
+ * Addition operator.
+ * @param lhs                       Left hand object.
+ * @param rhs                       Right hand object.
+ * @return New point from addition lhs and rhs.
+ */
 Point operator+(const Point& lhs, const Point& rhs);
 
+/**
+ * Subtraction operator.
+ * @param lhs                       Left hand object.
+ * @param rhs                       Right hand object.
+ * @return New point from subtraction lhs and rhs.
+ */
 Point operator-(const Point& lhs, const Point& rhs);
 
 } // namespace calgopp::types
