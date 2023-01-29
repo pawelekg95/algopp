7f33c2a5ce3187bd5f161c3be1d960d587a57434
pawelekg95 <56723306+pawelekg95@users.noreply.github.com>
2023-01-29 21:18:40 +0000

    Deploying to gh-pages from @ pawelekg95/calgopp@b07fd32577680c8f40bc878778f5cc69c7e755dc 🚀

--- search/all_0.js
+++ search/all_0.js
@@ -2,9 +2,12 @@ var searchData=
 [
   ['a_0',['a',['../structcalgopp_1_1function_1_1LinearFunctionImpl.html#a186b54da08026c9bd52bec9968e41726',1,'calgopp::function::LinearFunctionImpl::a()'],['../classcalgopp_1_1function_1_1LinearFunction.html#a0c1ba9f2dd3c72a4290adf5e4d5919ef',1,'calgopp::function::LinearFunction::a()']]],
   ['abs_1',['abs',['../namespacecalgopp_1_1math.html#ac45ad0a916b2bd3640c5146605f9b8ee',1,'calgopp::math::abs(const Type &amp;number)'],['../namespacecalgopp_1_1math.html#a7d6a21001b16c801d61ad684bd65d912',1,'calgopp::math::abs(const types::Complex &amp;number)']]],
-  ['algorithm_2eh_2',['algorithm.h',['../algorithm_8h.html',1,'']]],
-  ['append_3',['append',['../classcalgopp_1_1signal_1_1Signal.html#a254af39f802db27d3b17b33ae4d93b67',1,'calgopp::signal::Signal::append()'],['../classcalgopp_1_1types_1_1Container.html#a9b930c2099db6b2ed0b7528b4c1eef33',1,'calgopp::types::Container::append()']]],
-  ['approximation_2ecpp_4',['approximation.cpp',['../approximation_8cpp.html',1,'']]],
-  ['approximation_2eh_5',['approximation.h',['../approximation_8h.html',1,'']]],
-  ['at_6',['at',['../classcalgopp_1_1types_1_1Container.html#ae8d7a42afa6b59610c981f862a979a63',1,'calgopp::types::Container']]]
+  ['acos_2',['acos',['../namespacecalgopp_1_1math.html#a31d80effff34b6ac72cdaeef779857fa',1,'calgopp::math']]],
+  ['algorithm_2eh_3',['algorithm.h',['../algorithm_8h.html',1,'']]],
+  ['append_4',['append',['../classcalgopp_1_1signal_1_1Signal.html#a254af39f802db27d3b17b33ae4d93b67',1,'calgopp::signal::Signal::append()'],['../classcalgopp_1_1types_1_1Container.html#a9b930c2099db6b2ed0b7528b4c1eef33',1,'calgopp::types::Container::append()']]],
+  ['approximation_2ecpp_5',['approximation.cpp',['../approximation_8cpp.html',1,'']]],
+  ['approximation_2eh_6',['approximation.h',['../approximation_8h.html',1,'']]],
+  ['asin_7',['asin',['../namespacecalgopp_1_1math.html#a2f4303b8e218d1eca233d36ea329e9d0',1,'calgopp::math']]],
+  ['at_8',['at',['../classcalgopp_1_1types_1_1Container.html#ae8d7a42afa6b59610c981f862a979a63',1,'calgopp::types::Container']]],
+  ['atan_9',['atan',['../namespacecalgopp_1_1math.html#af18f3653cecc52fac80953bbd83eb392',1,'calgopp::math']]]
 ];
